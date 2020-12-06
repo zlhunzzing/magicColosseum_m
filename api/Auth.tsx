bdf71c39b532afb57549184ee6d686514c74072d
zlhunzzing <zlhunzzing@gmail.com>
2020-12-06 14:34:19 +0900

    회원가입 추가

--- api/Auth.tsx
+++ api/Auth.tsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import store from '../redux';
 import * as authActions from '../redux/Auth';
+import { envServer } from '../env'
 
-// const serverIp = 'localhost';
-const serverIp = '10.0.2.2:3000';
+const serverIp = envServer
+// const serverIp = '10.0.2.2:3000';
 
 export function signin(email: string, password: string, navigation: any) {
   return axios
@@ -16,6 +17,24 @@ export function signin(email: string, password: string, navigation: any) {
       store.dispatch(authActions.user_sign_in());
       store.dispatch(authActions.set_user_id({ userId: res.data.id }));
       navigation.navigate('Home');
+    })
+    .catch((err) => console.log(err.response));
+}
+
+export function signup(
+  email: string,
+  password: string,
+  username: string,
+  navigation: any,
+) {
+  return axios
+    .post(`http://${serverIp}/user/signup`, {
+      email,
+      password,
+      username,
+    })
+    .then((res) => {
+      navigation.navigate('Intro');
     })
     .catch((err) => console.log(err.response));
 }
--- page/Intro.tsx
+++ page/Intro.tsx
@@ -4,13 +4,19 @@ import { TextInput } from 'react-native-gesture-handler';
 import * as api from '../api/Auth'
 
 export default function Intro({ navigation }: any) {
+  const [isSignup, setIsSignup] = React.useState(false)
   const [email, setEmail] = React.useState('')
   const [password, setPassword] = React.useState('')
+  const [username, setUsername] = React.useState('')
 
   return (
     <View style={style.container}>
-      <Text style={style.title}>검투의 요정</Text>
-      <View style={style.form}>
+      <View style={style.header}>
+        <Text style={{ fontSize: 30 }}>검투의 요정</Text>
+      </View>
+      {!isSignup ? (
+        <View style={style.form}>
+        <Text style={{ fontSize: 20 }}>로그인</Text>
         <TextInput
           style={style.input}
           placeholder="example@example.com"
@@ -26,18 +32,58 @@ export default function Intro({ navigation }: any) {
           }}
         ></TextInput>
         <Button
-          title="log in"
+          title="로그인"
           onPress={() => {
             api.signin(email, password, navigation)
           }}
         />
+        <View style={{ margin: 3 }}></View>
+        <Button
+          title="회원가입"
+          onPress={() => {
+            setIsSignup(true)
+          }}
+          />
       </View>
-      <View style={{ margin: 3 }}></View>
-      <Button
-        title="sign up"
-        onPress={()=> {
-          // e.preventDefault()
-        }}></Button>
+      ) : (
+        <View style={style.form}>
+        <Text style={{ fontSize: 20 }}>회원가입</Text>
+        <TextInput
+          style={style.input}
+          placeholder="example@example.com"
+          onChangeText={(email) => {
+            setEmail(email)
+          }}
+        ></TextInput>
+        <TextInput
+          style={style.input}
+          placeholder="password"
+          onChangeText={(password) => {
+            setPassword(password)
+          }}
+        ></TextInput>
+        <TextInput
+          style={style.input}
+          placeholder="username"
+          onChangeText={(username) => {
+            setUsername(username)
+          }}
+        ></TextInput>
+        <Button
+          title="뒤로가기"
+          onPress={() => {
+            setIsSignup(false)
+          }}
+        />
+        <View style={{ margin: 3 }}></View>
+        <Button
+          title="가입하기"
+          onPress={() => {
+            api.signup(email, password, username, setIsSignup)
+          }}
+        />
+        </View>
+     )}
     </View>
   );
 }
@@ -46,25 +92,24 @@ const style = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: 'white',
   },
-  title: {
-    padding: 60,
-    fontSize: 30,
+  header: {
+    flex: 1,
+    justifyContent: 'center',
   },
   form: {
+    flex: 2,
     alignItems: 'center',
-    padding: 5,
-    backgroundColor: 'rgb(245, 245, 245)',
-    width: 300,
-    height: 140,
   },
   input: {
     margin: 3,
     textAlignVertical: 'top',
     borderWidth: 1,
-    width: 280,
-    height: 40,
+    width: 250,
+    height: 30,
     padding: 5,
+    backgroundColor: 'rgb(245, 245, 245)'
   },
 });
